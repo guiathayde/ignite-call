8c98ba6ff72bd0c5cefa2a8eb545d536bde5e059
Guilherme Athayde <guiathayde303@gmail.com>
2023-06-27 13:46:14 -0300

    Add ClaimUsernameForm component and install React Hook Form

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -7,7 +7,3 @@ globalStyles();
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-/*
-Utilizando nosso Design System, vamos criar uma aplicação completa até o deploy. 
-Abordaremos rotas autenticadas no Next.js, integração com Google Calendar, validações e muito mais!
-*/
