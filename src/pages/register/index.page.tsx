bb9a199431dba492a8570f24f658116b3c2f03e8
Guilherme Athayde <guiathayde303@gmail.com>
2023-07-18 13:07:51 -0300

    :clown_face: Error in next-auth setup

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -47,6 +47,8 @@ export default function Register() {
         name,
         username,
       });
+
+      await router.push('/register/connect-calendar');
     } catch (error) {
       console.error(error);
 
